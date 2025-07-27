0f90d4b902ac9cfb0f59111fbe97a3999ca7b0fb
sakib667788 <your-email-associated-with-sakib667788>
2025-07-28 02:12:33 +0600

    ss

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  publicDir: 'public',
+  build: { outDir: 'dist' }
 })
